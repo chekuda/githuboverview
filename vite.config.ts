47583adcbfab386204ae97c391243a776bfa7882
jose <joselchecaexpo@gmail.com>
2025-09-19 20:49:29 +0200

    add base on vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/githuboverview/",
   test: {
     // 👋 add the line below to add jsdom to vite
     environment: "jsdom",
